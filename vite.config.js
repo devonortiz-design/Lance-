612531e9bcd9811a161c236369243040a03b1389
devonortiz-design <devon.ortiz@gmail.com>
2026-07-25 21:44:40 -0700

    Simplify vite config

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  esbuild: {
-    charset: 'utf8',
-  },
-  define: {
-    'process.env': {}
-  },
   build: {
     outDir: 'build',
-    charset: 'utf8'
   }
 })
