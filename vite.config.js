db6cfc27cd540a43339e4afaea7b6e87a043c854
devonortiz-design <devon.ortiz@gmail.com>
2026-08-02 08:43:43 -0700

    Revert to last good build (20194a99): vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,3 @@
-// v1785395756
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -6,6 +5,5 @@ export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'build',
-    emptyOutDir: true,
   }
 })
